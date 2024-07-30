7dde6febfffa5c18c9790794db3c7ac5bec1be75
yousefturin <yusefturin@gmail.com>
2024-07-30 21:14:52 +0300

    Refactor InformationInDashboard.js to include selectedBtnMainActions prop

--- src/component/InformationInDashboard/InformationInDashboard.js
+++ src/component/InformationInDashboard/InformationInDashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./InformationInDashboard.css"
 import CountUp from 'react-countup';
 
-function InformationInDashboard({ informationAbout }) {
+function InformationInDashboard({ informationAbout, selectedBtnMainActions }) {
     return (
         <div className="info-card-main">
             {informationAbout.map((info, index) => {
