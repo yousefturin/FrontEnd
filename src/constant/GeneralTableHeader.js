21ecc11d025d32fc182e3632e4248287177c190b
yousefturin <yusefturin@gmail.com>
2024-07-30 12:24:17 +0300

    feat: Add GeneralTableHeader constant for table header titles

---
+++ src/constant/GeneralTableHeader.js
@@ -0,0 +1,27 @@
+const GeneralTableHeaderTitle = [
+    {
+        keyId: 'Dashboard',
+        title: 'Last Transactions',
+    },
+    {
+        keyId: 'Books',
+        title: 'Books Records',
+    },
+    {
+        keyId: 'Members',
+        title: 'Members Records',
+    },
+    {
+        keyId: 'Authors',
+        title: 'Authors Records',
+    },
+    {
+        keyId: 'Publishers',
+        title: 'Publishers Records',
+    },
+    {
+        keyId: 'Switch',
+        title: 'Recent Transactions',
+    },
+];
+export { GeneralTableHeaderTitle };
