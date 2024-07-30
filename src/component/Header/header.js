41402633d3d309da3e8c2f8624c6132b689400b5
yousefturin <yusefturin@gmail.com>
2024-07-30 21:14:44 +0300

    Refactor header search input styles for better user experience

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -1,7 +1,46 @@
-import React from 'react'
-import "./header.css"
-import SvgComponent from '../../utils/SvgComponent'
+import React from 'react';
+import "./header.css";
+import SvgComponent from '../../utils/SvgComponent';
+
 function Header({ title, searchData }) {
+    const [optionIndex, setOptionIndex] = React.useState(0);
+    const [placeholderClass, setPlaceholderClass] = React.useState('placeholder-show');
+    const [isAnimating, setIsAnimating] = React.useState(true);
+
+    const options = [
+        "Type Book name",
+        "Type Author",
+        "Type book title",
+        "Find an author",
+        "Search a book",
+        "Find a borrow"
+    ];
+
+    React.useEffect(() => {
+        let animationInterval;
+        let resetTimeout;
+
+        if (isAnimating) {
+            animationInterval = setInterval(() => {
+                setPlaceholderClass('placeholder-hide');
+                setTimeout(() => {
+                    setOptionIndex((prevIndex) => (prevIndex + 1) % options.length);
+                    setPlaceholderClass('placeholder-show');
+                }, 500); // Match with CSS animation duration
+            }, 5000); // Duration of each animation
+
+            resetTimeout = setTimeout(() => {
+                setIsAnimating(false);
+                setPlaceholderClass(''); // Reset class to default
+            }, 600000); // 10 minutes in milliseconds
+        }
+
+        return () => {
+            clearInterval(animationInterval);
+            clearTimeout(resetTimeout);
+        };
+    }, [isAnimating]);
+
     return (
         <div>
             <div className="header">
@@ -16,11 +55,15 @@ function Header({ title, searchData }) {
                         stroke={"#A0A0A0"}
                         title='search icon'
                     />
-                    <input type="text" placeholder="Search"  />
+                    <input
+                        type="text"
+                        placeholder={isAnimating ? options[optionIndex] : "Search"}
+                        className={placeholderClass}
+                    />
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
