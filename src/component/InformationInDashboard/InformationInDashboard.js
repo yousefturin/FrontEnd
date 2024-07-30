b40e9f2f6b792396cfac29339ad9ceae00826c4a
yousefturin <yusefturin@gmail.com>
2024-07-30 12:23:58 +0300

    chore: Update InformationInDashboard component to use react-countup for animated number display

--- src/component/InformationInDashboard/InformationInDashboard.js
+++ src/component/InformationInDashboard/InformationInDashboard.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import "./InformationInDashboard.css"
+import CountUp from 'react-countup';
 
 function InformationInDashboard({ informationAbout }) {
     return (
         <div className="info-card-main">
-            {informationAbout.map((info, index) => (
-                <div
-                    className="info-card"
-                    key={index}
-                >
-                    <h2>{info.num}</h2>
-                    <p>{info.title}</p>
-                </div>
-            ))}
+            {informationAbout.map((info, index) => {
+                return (
+                    <div className="info-card" key={index}>
+                        <h2>
+                            <CountUp
+                                start={0}
+                                end={info.num}
+                                duration={2.5} // Duration in seconds
+                                separator=","
+                            />
+                        </h2>
+                        <p>{info.title}</p>
+                    </div>
+                );
+            })}
         </div>
     )
 }
